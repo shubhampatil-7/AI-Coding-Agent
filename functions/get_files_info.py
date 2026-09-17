3ccc891bfa1e2d6e225a69fb60264d617d59fc17
Shubham <patilshubham821@gmail.com>
2026-09-17 14:33:55 +0000

    changes

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,2 +1,28 @@
+import os
+
+
 def get_files_info(working_directory: str, directory: str = ".") -> str:
-    pass
+    try:
+        abs_working_dir = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(abs_working_dir, directory))
+        if os.path.commonpath([abs_working_dir, target_dir]) != abs_working_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+        
+    
+
+        files_info = []
+
+        for filename in os.listdir(target_dir):
+            file_path = os.path.join(target_dir, filename)
+            is_dir = os.path.isdir(file_path)
+            file_size = os.path.getsize(file_path)
+            files_info.append(
+                f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}"
+            )
+
+        return "\n".join(files_info)
+
+    except Exception as e:
+        return f"Error: {e}"
